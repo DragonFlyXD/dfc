592d671975da25ae0adef640bb1e762af3847a88
chenlongfei <dragonfly920130@outlook.com>
2020-11-11 00:25:15 +0800

    添加控制器基类

--- dfcb-components/dfbc-common-core/src/main/java/com/dragonflyxd/dfcb/components/common/web/controller/BaseController.java
+++ dfcb-components/dfbc-common-core/src/main/java/com/dragonflyxd/dfcb/components/common/web/controller/BaseController.java
@@ -3,6 +3,11 @@ package com.dragonflyxd.dfcb.components.common.web.controller;
 import com.dragonflyxd.dfcb.components.common.dao.entity.BaseEntity;
 import com.dragonflyxd.dfcb.components.common.service.BaseService;
 import com.dragonflyxd.dfcb.components.common.web.dto.BaseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 控制器 - 基类
@@ -11,4 +16,24 @@ import com.dragonflyxd.dfcb.components.common.web.dto.BaseDTO;
  * @since 2020.11.09
  **/
 public abstract class BaseController<E extends BaseEntity, D extends BaseDTO, S extends BaseService<E, D>> {
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+
+    @Autowired
+    private S service;
+
+    @GetMapping("{id}")
+    public D show(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public D store(@RequestBody D dto) {
+        return service.save(dto);
+    }
+
+    @PutMapping("{id}")
+    public D update(@RequestBody D dto) {
+        return service.update(dto);
+    }
 }
