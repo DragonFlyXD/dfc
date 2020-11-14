5761c6911d0773ac9b4bda2f523f7d607b217e82
chenlongfei <dragonfly920130@outlook.com>
2020-11-14 16:56:00 +0800

    新增网关工程

---
+++ dfcb-platform/dfcb-gateway/src/main/java/com/dragonflyxd/dfcb/platform/gateway/DfcbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dragonflyxd.dfcb.platform.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfcbGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfcbGatewayApplication.class, args);
+    }
+
+}
---
+++ dfcb-platform/dfcb-gateway/src/test/java/com/dragonflyxd/dfcb/platform/gateway/DfcbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonflyxd.dfcb.platform.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfcbGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
